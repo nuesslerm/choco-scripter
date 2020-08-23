b7d78f4876fdf7dfa89282400d9e8bb190a10698
nuesslerm <markus@choco.com>
2020-08-23 16:16:30 +0200

    referenced homedir with os

--- src/helpers/loadChocoConfig.js
+++ src/helpers/loadChocoConfig.js
@@ -1,8 +1,9 @@
 const fs = require('fs-extra');
+const os = require('os');
 
 const loadChocoConfig = async () => {
   try {
-    return await fs.readJSON('/Users/nuesslerm/.choco/config.json', 'utf-8');
+    return await fs.readJSON(`${os.homedir}/.choco/config.json`, 'utf8');
   } catch (err) {
     if (err) throw err;
   }
